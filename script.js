61782e3efab5332f53c4850428454de2e2514bfb
Shweta Pawar <154500275+shwetapawar1@users.noreply.github.com>
2025-10-27 15:57:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ function updateTimer() {
+      const now = new Date();
+      const date = now.toLocaleDateString();
+      const time = now.toLocaleTimeString();
+      document.getElementById("timer").textContent = `${date} ${time}`;
+    }
+
+   
+    setInterval(updateTimer, 1000);
+
+   
+    updateTimer();
